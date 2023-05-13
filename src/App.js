d54cb5dd716ca5bd0e89f1efca1bfde1278e288a
tori-parmz <119701826+tori-parmz@users.noreply.github.com>
2023-05-13 14:42:52 -0500

    first change made

--- src/App.js
+++ src/App.js
@@ -9,6 +9,9 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
+        <p>
+          V2 of the App
+        </p>
         <a
           className="App-link"
           href="https://reactjs.org"
